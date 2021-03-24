1a0dbd52333c6fdae68b1bc51b62efa79fa722f1
Glitch (fglremaster) <none>
2021-03-24 14:45:59 +0000

    Glitch Export Code

---
+++ main/references/script.js
@@ -0,0 +1,55 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.replace("https://fgldesk.glitch.me/main/mobile.html");
+}
+var x = Math.floor((Math.random() * 10) + 1);
+if (x == 1)
+  {
+    console.log("Ejecti 499!");
+  }
+else if (x == 2)
+  {
+    console.log("Parabolus kiffofus!");
+  }
+else if (x == 3)
+  {
+    console.log("Lucy Simulator 3 confirmed. Initiate Fandom Wiki.");
+  }
+else if (x == 4)
+  {
+    console.log("What is this place?");
+  }
+else if (x == 5)
+  {
+    console.log("stop");
+  }
+else if (x == 6)
+  {
+    console.log("Esterbunny makes great project");
+  }
+else if (x == 7)
+  {
+    console.log("Do you like Dublin Dr Pepper?");
+  }
+else if (x == 8)
+  {
+    console.log("Enter passcode: ********");
+  }
+else if (x == 9)
+  {
+    console.log("A glass of milk. How intimidating.");
+  }
+else if (x == 10)
+  {
+    console.log("Do you hablo espanish?");
+  }
+else
+  {
+    console.log("Yong-ui-ja");
+  }
+
+const readline = require('readline');
+var y = readline();
+
+if (y == "hello world"){
+    console.log("lorem ipsum");
+  }
